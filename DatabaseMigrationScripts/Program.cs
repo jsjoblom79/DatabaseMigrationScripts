a6d0011862c6fae6f736d3a6f9ff0b4e27075e3e
jsjoblom79 <38762223+jsjoblom79@users.noreply.github.com>
2024-09-16 07:22:58 -0600

    Add project files.

---
+++ DatabaseMigrationScripts/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DatabaseMigrationScripts;
+using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
+using Microsoft.SqlServer.Management.Smo;
+using System.Collections.Specialized;
+using System.Text;
+
+Console.WriteLine("Please Enter the server you wish to connect to: ");
+var myserver = "192.168.1.140";
+Console.WriteLine("Please Enter the user ID:");
+var userId = "sa";
+Console.WriteLine("Please Enter the password:");
+var pass = "Calvin@rden10";
+//var connection = new SqlConnection($"Server={myserver};Database=master; User ID={userId};Password={pass};Trust Server Certificate=True");
+Console.WriteLine("Enter the directory to store the Scripts:");
+var outputDirectory = @"C:\DatabaseScripts";
+var connectionString = $"Server={myserver};Database=master; User ID={userId};Password={pass};Trust Server Certificate=True";
+
+
+using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+{
+    ServerConnection serverConnection = new ServerConnection(sqlConnection);
+    Server server = new Server(serverConnection);
+    var dbResults = ServerFunctions.GetDatabaseScripts(server.Databases, new Scripter(server));
+    foreach (Database db in server.Databases)
+    {
+        if (!db.IsSystemObject)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Database {db.Name} Started. {DateTime.Now:MM/dd/yyyy hh:mm:ss}");
+            Scripter scripter = new Scripter(server);
+         
+            scripter.Options.ScriptDrops = false;
+            scripter.Options.WithDependencies = false;
+            scripter.Options.ScriptSchema = true;
+            scripter.Options.ScriptData = false;
+            scripter.Options.IncludeHeaders = true;
+            scripter.Options.SchemaQualify = true;
+            scripter.Options.ScriptBatchTerminator = true;
+            
+
+            string outputFile = Path.Combine(outputDirectory, $"{db.Name}_SchemaObjects.sql");
+
+            TableCollection tables = db.Tables;
+            StoredProcedureCollection storedProcedures = db.StoredProcedures;
+            UserDefinedFunctionCollection functions = db.UserDefinedFunctions;
+            ViewCollection views = db.Views;
+
+            StringBuilder scriptLines = new();
+            scriptLines.AppendLine($"IF NOT EXISTS (SELECT name FROM master.sys.databases WHERE name = '{db.Name}')\nCREATE DATABASE {db.Name}");
+            scriptLines.AppendLine($"USE {db.Name}\nGO");
+            //StringCollection? tableScript;// = new StringCollection();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Getting Table Scripts.");
+            scriptLines.AppendLine(ServerFunctions.GetTableScripts(tables,scripter));
+            Console.WriteLine("Getting Procedure Scripts.");
+            
+            scriptLines.AppendLine(ServerFunctions.GetProcedureScripts(storedProcedures,scripter));
+            Console.WriteLine("Getting Function Scripts.");
+            scriptLines.AppendLine(ServerFunctions.GetFunctionScripts(functions, scripter));
+            Console.WriteLine("Getting View Scripts.");
+            scriptLines.AppendLine(ServerFunctions.GetViewScripts(views, scripter));
+
+
+
+            File.WriteAllText(outputFile, scriptLines.ToString());
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Database {db.Name} has been completed. {DateTime.Now:MM/dd/yyyy hh:mm:ss}");
+
+        }
+    }
+}
+Console.ForegroundColor = ConsoleColor.White;
+
+
+
---
+++ DatabaseMigrationScripts/ServerFunctions.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.SqlServer.Management.Common;
+using Microsoft.SqlServer.Management.Sdk.Sfc;
+using Microsoft.SqlServer.Management.Smo;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.SqlServer.Management.Dmf.ExpressionNodeFunction;
+
+namespace DatabaseMigrationScripts
+{
+    public class ServerFunctions
+    {
+        public static DataSet QueryServer(string host, string userId, string pass,string dbName)
+        {
+            DataSet result = new DataSet();
+            var connectionString = $"Server={host};Database={dbName};User ID={userId};Password={pass};Trust Server Certificate=True";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                var serverConnection = new ServerConnection(connection);
+                Server server = new Server(serverConnection);
+                 result = server.ConnectionContext.ExecuteWithResults($"SELECT name FROM sys.procedures WHERE name not LIKE 'sp_%'");
+            }
+            return result;
+
+        }
+        internal static string? GetFunctionScripts(UserDefinedFunctionCollection functions, Scripter scripter)
+        {
+            string funcInfo = "";
+            foreach (UserDefinedFunction func in functions)
+            {
+                if (!func.IsSystemObject)
+                {
+                    var cnt = 0;
+                    var procScript = scripter.Script(new Urn[] { func.Urn });
+
+                    foreach (var line in procScript)
+                    {
+                        if(cnt == procScript.Count - 1)
+                        {
+                            funcInfo += $"{line}' EXEC (@var_{func.Name}) END\n";
+                        }
+                        else
+                        {
+                            funcInfo += $"{line}\n";
+                        }
+                        
+                        if (cnt == 1)
+                        {
+                            funcInfo += $"IF NOT EXISTS (SELECT name FROM sys.objects WHERE name='{func.Name}')\nBEGIN\n" +
+                                $"DECLARE @var_{func.Name} VARCHAR(MAX) = \n'";
+                        }
+                        cnt++;
+                    }
+                }
+            }
+            return funcInfo;
+        }
+
+        internal static string? GetProcedureScripts(StoredProcedureCollection storedProcedures, Scripter scripter)
+        {
+            string procInfo = "";
+
+
+            foreach (StoredProcedure proc in storedProcedures)
+            {
+                if (!proc.IsSystemObject)
+                {
+                    var cnt = 0;
+                    var procScript = scripter.Script(new Urn[] { proc.Urn });
+
+                    foreach (var line in procScript)
+                    {
+                        if(cnt == procScript.Count - 1)
+                        {
+                            procInfo += $"{line}' EXEC(@var_{proc.Name}) END\n";
+                        }
+                        else
+                        {
+                            procInfo += $"{line}\n";
+                        }
+                        
+                        if (cnt == 1)
+                        {
+                            procInfo += $"IF NOT EXISTS (SELECT name FROM sys.procedures WHERE name='{proc.Name}')\n" +
+                                $"BEGIN\nDECLARE @var_{proc.Name} VARCHAR(MAX) =\n'";
+                        }
+                        cnt++;
+                    }
+                }
+            }
+            return procInfo;
+        }
+
+        internal static string? GetTableScripts(TableCollection tables, Scripter scripter)
+        {
+            string tableinfo = "";
+            foreach (Table table in tables)
+            {
+                if (!table.IsSystemObject)
+                {
+                    var cnt = 0;
+                    var tableScript = scripter.Script(new Urn[] { table.Urn });
+
+                    foreach (var line in tableScript)
+                    {
+                        tableinfo += $"{line}\n";
+                        if (cnt == 1)
+                        {
+                            tableinfo += $"IF NOT EXISTS (SELECT name FROM sys.tables WHERE name='{table.Name}')\n";
+                        }
+                        cnt++;
+                    }
+                }
+            }
+            return tableinfo;
+        }
+
+        internal static string? GetViewScripts(ViewCollection views, Scripter scripter)
+        {
+            string viewInfo = "";
+            foreach (View view in views)
+            {
+                if (!view.IsSystemObject)
+                {
+                    var cnt = 0;
+                    var procScript = scripter.Script(new Urn[] { view.Urn });
+
+                    foreach (var line in procScript)
+                    {
+                        if (cnt == procScript.Count-1)
+                        {
+                            viewInfo += $"{line}'\nEXEC (@var_{view.Name.Replace(" ","_")}) END\n";
+                        }
+                        else
+                        {
+                            viewInfo += $"{line}\n";
+                        }
+  
+                        if (cnt == 1)
+                        {
+                            viewInfo += $"IF NOT EXISTS (SELECT name FROM sys.views WHERE name='{view.Name}')\n" +
+                                $"BEGIN\nDECLARE @var_{view.Name.Replace(" ", "_")} VARCHAR(MAX) = '\n";
+                        }
+                        cnt++;
+                    }
+                }
+            }
+            return viewInfo;
+        }
+
+        public static string? GetDatabaseScripts(DatabaseCollection databases, Scripter scripter)
+        {
+            var dbInfo = "";
+            foreach (Database database in databases)
+            {
+                if (!database.IsSystemObject)
+                {
+                    var dbScript = scripter.Script(new Urn[] {database.Urn});
+                    foreach (var line in dbScript)
+                    {
+                        dbInfo += line;
+                    }
+                   // File.WriteAllText(@$"C:\DatabaseScripts\{database.Name}_DBSchema.sql", dbInfo);
+                }
+            }
+            return dbInfo;
+        }
+    }
+}
